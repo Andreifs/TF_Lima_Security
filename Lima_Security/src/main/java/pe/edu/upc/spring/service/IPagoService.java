217a00edadfe786e9c6f100c1200394e9a8b4a8b
Juan Jose <48858530+jjmb9022@users.noreply.github.com>
2021-05-29 15:29:28 -0500

    Add files via upload

---
+++ Lima_Security/src/main/java/pe/edu/upc/spring/service/IPagoService.java
@@ -0,0 +1,17 @@
+package pe.edu.upc.spring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import pe.edu.upc.spring.model.Pago;
+
+public interface IPagoService {
+	public boolean insertar(Pago pago);
+	public boolean modificar(Pago pago);
+	public void eliminar(int idPago);
+	public Optional<Pago> listarId(int idPago);
+	public Optional<Pago> buscarId(int idPago);
+	List<Pago> listar();
+	
+
+}
