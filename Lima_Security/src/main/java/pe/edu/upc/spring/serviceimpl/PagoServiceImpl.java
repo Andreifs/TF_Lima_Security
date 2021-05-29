bfc722f8a440f5f1769eda8ec8eff466db02918e
Juan Jose <48858530+jjmb9022@users.noreply.github.com>
2021-05-29 15:29:59 -0500

    Add files via upload

---
+++ Lima_Security/src/main/java/pe/edu/upc/spring/serviceimpl/PagoServiceImpl.java
@@ -0,0 +1,75 @@
+package pe.edu.upc.spring.serviceimpl;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import pe.edu.upc.spring.model.Pago;
+import pe.edu.upc.spring.repository.IPagoRepository;
+import pe.edu.upc.spring.service.IPagoService;
+
+@Service
+public class PagoServiceImpl implements IPagoService {
+
+	@Autowired
+	private IPagoRepository dPago;
+	
+	@Override
+	@Transactional
+	public boolean insertar(Pago pago) {
+		Pago objPago = dPago.save(pago);
+		if(objPago == null)
+			return false;
+		else
+			return true;
+	}
+
+	@Override
+	@Transactional
+	public boolean modificar(Pago pago) {
+		/*
+		boolean flag = false;
+		try {
+			dDueno.save(dueno);
+			flag = true;
+		}
+		catch(Exception ex) {
+			System.out.println("Sucedio un error...");
+		}
+		return flag;
+		*/
+		return true;
+	}
+
+	@Override
+	@Transactional
+	public void eliminar(int idPago) {
+		dPago.deleteById(idPago);
+		
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Pago> listarId(int idPago) {
+		return dPago.findById(idPago);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Pago> listar() {
+		return dPago.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Pago> buscarId(int idPago) {
+		return dPago.findById(idPago);
+	}
+
+	
+	
+}
