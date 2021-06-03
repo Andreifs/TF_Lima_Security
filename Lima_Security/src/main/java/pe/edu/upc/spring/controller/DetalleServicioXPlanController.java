e01da87decad1e3931adb0a1f1542e8c3e3e9aaf
JDBL2300 <jdiegobl@hotmail.com>
2021-06-03 16:33:57 -0500

    Entidad DetalleServicioXPlan
    Creacion de la entidad de DetalleServicioXPlan

---
+++ Lima_Security/src/main/java/pe/edu/upc/spring/controller/DetalleServicioXPlanController.java
@@ -0,0 +1,215 @@
+package pe.edu.upc.spring.controller;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.sun.el.parser.ParseException;
+
+import pe.edu.upc.spring.model.DetalleServicioXPlan;
+import pe.edu.upc.spring.model.Dueno;
+import pe.edu.upc.spring.model.Pet;
+import pe.edu.upc.spring.model.Servicio;
+import pe.edu.upc.spring.model.Plan;
+import pe.edu.upc.spring.model.Race;
+import pe.edu.upc.spring.service.IDetalleServicioXPlanService;
+import pe.edu.upc.spring.service.IDuenoService;
+import pe.edu.upc.spring.service.IPetService;
+import pe.edu.upc.spring.service.IServicioService;
+import pe.edu.upc.spring.service.IPlanService;
+import pe.edu.upc.spring.service.IRaceService;
+
+@Controller
+@RequestMapping("/detalleServicioXPlan")
+public class DetalleServicioXPlanController {
+	
+	@Autowired
+	private IDetalleServicioXPlanService dService;
+	
+	
+	@Autowired
+	private IRaceService rService;
+	@Autowired
+	private IDuenoService dService;
+	@Autowired
+	private IPetService pService;
+	
+	@RequestMapping("/bienvenido")
+	public String irPaginaBienvenida() {
+		return "bienvenido";
+	}
+	
+	@RequestMapping("/")
+	public String irPaginaListadoMascotas(Map<String,Object>model) {
+		model.put("listaMascotas", pService.listar());
+		return "listPets";
+	}
+	
+	@RequestMapping("/irRegistrar")
+	public String irPaginaRegistrar(Model model) {
+		
+		model.addAttribute("listaRazas", rService.listar());
+		model.addAttribute("listaDueno", dService.listar());
+		
+		model.addAttribute("race", new Race());
+		model.addAttribute("dueno", new Dueno());
+		model.addAttribute("pet", new Pet());
+		
+		return "pet";
+	}
+	
+	@RequestMapping("/registrar")
+	public String registrar (@ModelAttribute Pet objPet,BindingResult binRes,Model model)
+	throws ParseException
+		{
+			if(binRes.hasErrors()) {
+			model.addAttribute("listaRazas", rService.listar());
+			model.addAttribute("listaDueno", dService.listar());
+			return("pet");
+			}
+			else {
+				boolean flag = pService.insertar(objPet);
+				if (flag)
+					return "redirect:/pet/listar";
+				else {
+					model.addAttribute("mensaje", "Ocurrio un error");
+					return "redirect:/pet/irRegistrar";
+				}
+			}
+		}
+		
+	@RequestMapping("/modificar/{id}")
+	public String modificar(@PathVariable int id, Model model, RedirectAttributes objRedir) 
+		throws ParseException 
+	{
+		Optional<Pet> objPet = pService.listarId(id);
+		if(objPet == null) {
+			objRedir.addFlashAttribute("mensaje", "Ocurrio un error");
+			return "redirect:/pet/listar";
+		}
+		else {
+			model.addAttribute("listaRazas", rService.listar());
+			model.addAttribute("listaDueno", dService.listar());
+			
+			if(objPet.isPresent())
+				objPet.ifPresent(o -> model.addAttribute("pet" ,o ));
+			return "pet";
+		}
+	}		
+		
+	@RequestMapping("/eliminar")
+	public String eliminar(Map<String, Object> model, @RequestParam(value="id") Integer id) {
+		try {
+			if (id != null && id>0) {
+				pService.eliminar(id);
+				model.put("listaMascotas", pService.listar());
+			}
+		}
+		catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			model.put("mensaje", "Ocurrio un error");
+			model.put("listaMascotas", pService.listar());
+		}
+		return "listPets";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Map<String, Object> model) {
+		model.put("listaMascotas", pService.listar());
+		return "listPets";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@RequestMapping("/bienvenido")
+	public String irPaginaBienvenida() {
+		return "bienvenido";
+	}
+	
+	@RequestMapping("/")
+	public String irPaginaListadoServicios(Map<String, Object> model) {
+		model.put("listaServicios", sService.listar());
+		return "listServicio";
+	}
+	
+	@RequestMapping("/irRegistrar")
+	public String irPaginaRegistrar(Model model) {
+		model.addAttribute("servicio", new Servicio());
+		return "servicio";
+	}
+	
+	@RequestMapping("/registrar")
+	public String registrar(@ModelAttribute Servicio objServ, BindingResult binRes, Model model) throws ParseException {
+		if (binRes.hasErrors())
+			return "servicio";
+		else {
+			boolean flag = sService.insertar(objServ);
+			if (flag)
+				return "redirect:/servicio/listar";
+			else {
+				model.addAttribute("mensaje", "Ocurrio un error");
+				return "redirect:/servicio/irRegistrar";
+			}
+		}
+	}
+	
+	@RequestMapping("/modificar/{id}")
+	public String modificar(@PathVariable int id, Model model, RedirectAttributes objRedir) throws ParseException {
+		Optional<Servicio> objDisp = sService.listarId(id);
+		if(objDisp == null) {
+			objRedir.addFlashAttribute("mensaje", "Ocurrio un error");
+			return "redirect:/servicio/listar";
+		}
+		else {
+			model.addAttribute("servicio", objDisp);
+			return "servicio";
+		}
+	}
+	
+	@RequestMapping("/eliminar")
+	public String eliminar(Map<String, Object> model, @RequestParam(value="id") Integer id) {
+		try {
+			if (id != null && id>0) {
+				sService.eliminar(id);
+				model.put("listaServicios", sService.listar());
+			}
+		}
+		catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			model.put("mensaje", "Ocurrio un error");
+			model.put("listaServicios", sService.listar());
+		}
+		return "listServicio";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Map<String, Object> model) {
+		model.put("listaServicios", sService.listar());
+		return "listServicio";
+	}
+	
+	
+	
+	
+}
---
+++ Lima_Security/src/main/java/pe/edu/upc/spring/serviceimpl/DetalleServicioXPlanServiceImpl.java
@@ -0,0 +1,81 @@
+package pe.edu.upc.spring.serviceimpl;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import pe.edu.upc.spring.service.IDetalleServicioXPlanService;
+import pe.edu.upc.spring.model.DetalleServicioXPlan;
+import pe.edu.upc.spring.repository.IDetalleServicioXPlanRepository;
+
+
+public class DetalleServicioXPlanServiceImpl implements IDetalleServicioXPlanService {
+
+	
+	@Autowired
+	private IDetalleServicioXPlanRepository dDetalleServicioXPlan;
+
+	@Override
+	@Transactional
+	public boolean insertar(DetalleServicioXPlan detalleServicioXPlan) {
+		DetalleServicioXPlan objDetalleServicioXPlan= dDetalleServicioXPlan.save(detalleServicioXPlan);
+		if	(objDetalleServicioXPlan !=null)
+			return true;
+		else
+			return false;	
+	}
+
+	@Override
+	@Transactional
+	public boolean modificar(DetalleServicioXPlan detalleServicioXPlan) {
+		boolean flag = false;
+		try {
+			dDetalleServicioXPlan.save(detalleServicioXPlan);
+			flag=true;
+		}
+		catch(Exception ex) {
+			
+			System.out.println(ex.getMessage());
+		}
+		return flag;
+	}
+
+	@Override
+	@Transactional
+	public void eliminar(int idDetalleServicioXPlan) {
+		dDetalleServicioXPlan.deleteById(idDetalleServicioXPlan);
+		
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<DetalleServicioXPlan> listar() {
+		return dDetalleServicioXPlan.findAll();
+	}
+
+	@Override
+	@Transactional
+	public List<DetalleServicioXPlan> buscarPlan(String namePlan) {
+		return dDetalleServicioXPlan.buscaPlan(namePlan);
+	}
+
+	@Override
+	@Transactional
+	public List<DetalleServicioXPlan> buscarServicio(String nameServicio) {
+		return dDetalleServicioXPlan.buscarServicio(nameServicio);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
