a8bf4e24ad1b87d430ea2df57e134e2e21c16f96
JDBL2300 <jdiegobl@hotmail.com>
2021-06-27 14:03:27 -0500

    Login y Logout parte 1

--- Lima_Security/src/main/java/pe/edu/upc/spring/SpringSecurityConfig.java
+++ Lima_Security/src/main/java/pe/edu/upc/spring/SpringSecurityConfig.java
@@ -3,14 +3,17 @@ package pe.edu.upc.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 import pe.edu.upc.spring.auth.handler.LoginSuccessHandler;
 import pe.edu.upc.spring.serviceimpl.JpaUserDetailsService;
 
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	
 	@Autowired
 	private JpaUserDetailsService userDetailsService;
 	
@@ -31,15 +34,19 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/pago/**").access("hasRole('ROLE_ADMIN')or hasRole('ROLE_USER')")
 			.antMatchers("/plan/**").access("hasRole('ROLE_ADMIN')or hasRole('ROLE_USER')")
 			.antMatchers("/servicio/**").access("hasRole('ROLE_ADMIN')or hasRole('ROLE_USER')")
-			.antMatchers("/welcome/**").access("hasRole('ROLE_ADMIN')or hasRole('ROLE_USER')").and()
+			.antMatchers("/welcome/**").access("hasRole('ROLE_ADMIN')or hasRole('ROLE_USER')").and()			
 			.formLogin().successHandler(successHandler).loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/welcome/bienvenido")
 			.permitAll().and().logout().logoutSuccessUrl("/login").permitAll().and().exceptionHandling().accessDeniedPage("/error_403");
+			
 			
 		}
 		catch (Exception ex) {
 			System.out.println(ex.getMessage());
 		}
 	}
+	
+
+
 	
 	public void configureGlobal(AuthenticationManagerBuilder build) throws Exception{
 		build.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
