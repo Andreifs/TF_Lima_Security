68bb697b80e689d4ab6e45330ca7077cb830be6c
Andreifs <andreifalen@gmail.com>
2021-06-27 21:05:22 -0500

    Update SpringSecurityConfig.java

--- Lima_Security/src/main/java/pe/edu/upc/spring/SpringSecurityConfig.java
+++ Lima_Security/src/main/java/pe/edu/upc/spring/SpringSecurityConfig.java
@@ -1,15 +1,19 @@
 package pe.edu.upc.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 import pe.edu.upc.spring.auth.handler.LoginSuccessHandler;
 import pe.edu.upc.spring.serviceimpl.JpaUserDetailsService;
+
+@EnableGlobalMethodSecurity(securedEnabled=true)
+@Configuration
 
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
